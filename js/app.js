b7389899f3f4d904b9efc74e186bf478abd86608
BigDDawg <chris.dwyer@infinitydiscs.com>
2016-12-06 20:29:28 -0500

    Lastest

--- js/app.js
+++ js/app.js
@@ -1,29 +1,4 @@
 /*
- 1. Add all the DOM functionality first
-
-    API Key from:
-    http://www.npr.org/account/profile?status=verified&uidt=1479861635
- MDI4NjQyNzE4MDE0Nzk4NjE2NzRmMzgxMQ000
-
- Give the user the ability to pull from a multiple news sources. Here are two news sources we suggest:
-
-
- 2. Map out all of the needed fields/properties from each respective feed.
-
- 3. Start by doing a console.log of the incoming feeds to confirm you have a successful transaction before you start mapping anything out.
-
-
- 4. Make sure you have the JSON View chrome extension to get a clean view of the JSON dump in your browser.
-
- 5. Think about ways to best standardize all of your incoming data.
-
- 6. Test small pieces of functionality frequently, to make sure everything is working.
-
- Use tools such as Stack Overflow, Google and documentation resources to solve problems.
-
-*/
-
- /*
 
  If you use your own feeds, they must have APIs with the following minimum
  requirements:
@@ -59,6 +34,23 @@
  - Add an error message (either alert or a notification on the page) if the app
  cannot load from the selected feed.
 
+
+
+GET http://api.ft.com/content/search/field/golf/v1?apiKey=tbuyhewt793fcjwqc4sxqx75
+
+________
+Explanations of API requests.
+
+https://www.sitepoint.com/introduction-jquery-shorthand-ajax-methods/
+Go here for an error code Explanation
+http://codepen.io/SitePoint/pen/CwesD
+
+Go here for an error code Explanation
+http://codepen.io/SitePoint/pen/CwesD
+
+Go here for an error code Explanation
+http://codepen.io/SitePoint/pen/CwesD
+
  __Additional UI interaction rules:__
 
 4. When the user clicks/taps the search icon, expand the input box. Best approach
@@ -83,10 +75,9 @@
 
 // This should pull the APIs when the window is loaded automatically.
 
-window.onload = function () {
-   alert('loaded testing');
-};
-
+$(window).load(function() {
+   $("#bottom2").append("<h1>Window Test</h1>");
+});
 
 
 // 2. When you successfully retrieve information from
@@ -98,16 +89,129 @@ window.onload = function () {
 // the loading pop up when the user first selects the new source, and hide it on
 // success.
 //
+
 //
-// $(this).siblings().css("color", "yellow");
-// $("body section.container").css("color", "green");
-// hide();
-//
+// Guardian API: http://content.guardianapis.com/search?from-date=2016-09-26&to-date=2016-12-01&order-by=newest&use-date=published&show-elements=image&rights=subscription-databases&q=sports&api-key=5fcdcdfa-9e65-419e-9994-28fd69012e5f
+
+// CNN - API Key:
+
+
+
+
+$.ajax({
+   url: "http://content.guardianapis.com/search?from-date=2015-08-06&to-date=2016-12-06&order-by=newest&use-date=published&show-elements=all&show-fields=thumbnail&rights=developer-community&q=news&api-key=5fcdcdfa-9e65-419e-9994-28fd69012e5f",
+   success: function(result){
+
+      $("#article1 .impressions").append(result.response.pages);
+
+      // Var results = 
+      var results = result.response.results;
+
+      $("#article1 .articleContent a h3").append(results[1].webTitle);
+
+      $("#article1 .articleContent h6").append("Category: " + results[1].sectionName);
+    
+      $("#article1 section.featuredImage img").attr("src", results[1].fields.thumbnail);
+      
+      // function run() {
+      //    var results = result.response.results;
+      //    for(var i = 0; i < results.length; i++){
+      //       $("#article1 h3").append(results[i].webTitle);
+      //       $("#article1 h6").append("Category: " + results[i].sectionName);
+      //       $("#article1 h6").append("Category: " + results[i].sectionName);
+      //       };
+
+      //    }
+      }
+})
+
+
+// $.ajax({
+//    url: "http://api.npr.org/query?id=1055&fields=title,text,image,parent,summary,titles&requiredAssets=text,image&dateType=story&output=JSON&apiKey=MDI4NjQyNzE4MDE0ODA0NTU3Njc4OTNmNg000",
+
+//    success: function(result){
+//       $("#bottom2").append("<h3>"+result.version+"</h3>");
+//       // for(var i = 0; i < result.list.story; i++){
+//       //    $("#bottom2").append(
+//       //       "<h3>"+result.list.story[i].title+"</h3>"+
+//       //       "<h3>"+result.list.story[i].link+"</h3>"
+            
+//       //    );
+//       // }
+//    }
+// })
+
+$.ajax({
+   url:"https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json",
+   success:function(results){
+
+      $("#article2 h6").append("Category: " + results.data.meta.description);
+
+      var feed = results.data.feed;
+      $("#article2 h3").append(feed[0].content.title);
+
+      $("#article2 .impressions").append(feed[0].diggs.count);
+
+      $("#article2 section.featuredImage img").attr("src", feed[0].content.media.images[i].original_url);
+
+      // run();
+
+      // function run() {
+      //    var feed = results.data.feed;
+      //    for(var i = 0; i < feed.length; i++){
+      //       $("#bottom").append(
+      //       "<h3>"+feed[i].content.title+"</h3>"+
+      //       "<h6>"+feed[i].diggs.count+"</h6>"+
+      //       "<h6>"+feed[i].content.media.images[i].original_url+"</h6>"+
+      //       "<img src='"+feed[i].content.media.images[i].original_url+"'>"
+      //       );
+      //     };
+      // }
+   }
+})
+
+$.ajax({
+   url:"https://www.reddit.com/.json",
+   success:function(response){
+
+      $("#article3 h3").append(response.data.children[0].data.title);
+
+      $("#article3 h6").append("Category: " + response.data.children[0].data.subreddit);
+
+      $("#article3 .impressions").append(response.data.children[0].data.score);
+
+      $("#article3 section.featuredImage img").attr("src", response.data.children[0].data.thumbnail);
+
+      // data is variable that we use to access multiple object properties below.
+      
+      // for(var i = 0; i < data.length; i++){
+
+      //    $("#bottom2").append(
+
+      //       "<h3>"+data[i].data.title+"</h3>"+
+
+      //       "<img src='"+data[i].data.thumbnail+"'>"
+
+      //    );
+      // }
+   }
+})
+
 
 // on click a link a the top, the article should show and the others should hide,
 // when you click on another link at the top, the article should show and the other should hide.
 // !!! This should happen after you have done it once,......Link should display and link back to just
 // one article.
+//
+
+
+// Hide all the elements in the DOM that have a class of "article".
+
+// $("nav ul li a").click(function () {
+//    $(this).siblings("a").hide();
+// });
+
+// // NOT FUNCTIONING - PROPERLY.
 
 $("ul li a#article1").click(function(){
    $('article#article1').show();
@@ -128,31 +232,13 @@ $("ul li a#article3").click(function(){
    $('article#article3').show();
 });
 
-// Hide all the elements in the DOM that have a class of "article".
-
-// When the user 1. selects an article's title - H3 Tag - show the #popUp overlay.
-// 2. The content of the article must be inserted in the `.container` class inside--> `#popUp`.
-// 3. Remove the `.loader` class when toggling the article--> information in the pop-up.
-
-
-$("h3").click(function(){
-   $("div#popUp").toggleClass("loader hidden");
-   var $testHtml2 = $(this).parents(".article");
-   $("div.container h1").replaceWith($testHtml2);
-});
-
-// 4 . Change the link of the "Read more from source" button to that of the
-// respective article.
-
-// When below is clicked, this should show the complete article or should go outside to an article.
-//
-// <a href="#" class="popUpAction" target="_blank">Read more from source</a>
-
 
 // 5. When the user clicks/taps the search icon, expand the input box. Best approach
 // for this is to toggle the `.active` class for the `#search` container. If the
 // search input box is already expanded tapping the search icon again will close
 // the input. Pressing the "Enter" key should also close the opened input box.
+
+// NOT FUNCTIONING.
 
 $("img#searchClick").click(function(event){
    event.preventDefault();
@@ -160,41 +246,15 @@ $("img#searchClick").click(function(event){
 });
 
 
-$.ajax({
-   url:"http://content.guardianapis.com/search?order-by=newest&show-tags=impressions&show-elements=image&q=golf&api-key=85f4d306-2499-4efd-9db7-19018a16837c",
-   success:function(responseAPI){
-      for(var i = 0; i < responseAPI.response.results.length; i++){
-         $("#articleTest").append(
-            "<h3>"+responseAPI.response.results[i].webTitle+"</h3>"+
-            "<a>"+responseAPI.response.results[i].webUrl+"</a>"
-
-         );
-      }
-   }
-})
-
-
-$.ajax({
-   url:"http://content.guardianapis.com/search?order-by=newest&show-tags=impressions&show-elements=image&q=golf&api-key=85f4d306-2499-4efd-9db7-19018a16837c",
-   success:function(responseAPI){
-      for(var i = 0; i < responseAPI.response.results.length; i++){
-         $("#article1").first().append(
-            "<h1>"+responseAPI.response.results[i].webTitle+"</h1>"
-
-         );
-      }
-   }
-})
-
-$.ajax({
-url:"https://www.reddit.com/.json",
-   success:function(response){
-   for(var i = 0; i < response.data.children.length; i++){
-      $("#first").append(
-         "<h1>"+response.data.children[i].data.title+"</h1>"+
-         "<img src='"+response.data.children[i].data.thumbnail+"'>"
-      );
-   }
-}
-})
-
+// $("#article1 h3").click(function() {
+//  $("a.popUpAction").attr("");
+// )};
+//
+// $("#article2 h3").click(function() {
+//  $("a.popUpAction").attr("");
+// )};
+//
+// $("#article3 h3").click(function() {
+//  $("a.popUpAction").attr("");
+// )};
+//
